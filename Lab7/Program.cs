80f8aba7ac6f7f70a73dc1c8a5857815690c1220
samantha <33375727+samanthamazzola@users.noreply.github.com>
2018-07-24 09:20:47 -0400

    Update Program.cs

--- Lab7/Program.cs
+++ Lab7/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Text.RegularExpressions;
 
 namespace Lab7
@@ -22,12 +22,12 @@ namespace Lab7
             //write a program that validates email
             Console.Write("Please enter a valid email: ");
             string emailInput = Console.ReadLine();
-            while(!Regex.IsMatch(emailInput, @"^[a-zA-Z\d]{1,30}\@[a-zA-Z\d]{5,30}\..[a-z]{2,3}$"))
+            while(!Regex.IsMatch(emailInput, @"^[a-zA-Z\d]{1,30}\@[a-zA-Z\d]{5,30}\.[a-z]{2,3}$"))
             {
                 Console.WriteLine("Sorry, email is not valid!");
                 emailInput = Console.ReadLine();
             }
-            while (Regex.IsMatch(emailInput, @"^[a-zA-Z\d]{1,30}\@[a-zA-Z\d]{5,30}\..[a-z]{2,3}$"))
+            while (Regex.IsMatch(emailInput, @"^[a-zA-Z\d]{1,30}\@[a-zA-Z\d]{5,30}\.[a-z]{2,3}$"))
             {
                 Console.WriteLine("Email is valid!");
                 break;
@@ -43,44 +43,27 @@ namespace Lab7
             }
             while (Regex.IsMatch(phoneNumberInput, @"^\d{3}\-\d{3}\-\d{4}$"))
             {
-                Console.WriteLine();
-                phoneNumberInput = Console.ReadLine();
+                Console.WriteLine("Phone number is valid: "+phoneNumberInput);
+                break;
             }
+
+            //write a program that validates date
+            Console.Write("Please enter a valid date: ");
+            string dateInput = Console.ReadLine();
+            while (!Regex.IsMatch(dateInput, @"^\d{2}\/\d{2}\/\d{4}$"))
+            {
+                Console.WriteLine("Sorry, date is not valid!");
+                dateInput = Console.ReadLine();
+            }
+            while (Regex.IsMatch(dateInput, @"^\d{2}\/\d{2}\/\d{4}$"))
+            {
+                Console.WriteLine();
+                dateInput = Console.ReadLine();
+            }
+
             //check if the user input matches the pattern (regular expression)
             // class Regex (see above the using statement for the Regex class made above)
             // IsMatch (look at right click debug "quick fix"
-
-           
-
-            /*if (Regex.IsMatch(emailInput, @"^.{0,30}\@.{5,30}\..{2,3}$"))
-            {
-                Console.WriteLine("Email is valid!");
-            }
-           
-            if (Regex.IsMatch(phoneNumberInput, @"^\d{3}\-\d{3}\-\d{4}$"))
-            {
-                Console.Write(phoneNumberInput);
-            }
-
-            //validation
-            while (!Regex.IsMatch(userInput, @"^[A-Z{1}[a-zA-Z]{0,29}$"))
-            {
-                Console.WriteLine("Sorry, name is not valid!");
-            }
-            while (!Regex.IsMatch(emailInput, @"^[A-Z{1}[a-zA-Z]{0,29}$"))
-            {
-                Console.WriteLine("Sorry, email is not valid!");
-            }
-            */
-            /*while (!Regex.IsMatch(emailInput, @"^.{0,30}\@.{5,30}\..{2,3}$"))
-            {
-                Console.WriteLine("Nice try! next time please do it right!");
-            }   
-            while (!Regex.IsMatch(phoneNumberInput, @"^\d{3}\-\d{3}\-\d{4}$"))
-            {
-                Console.WriteLine("Nice try! next time please do it right!");
-            }
-            Console.WriteLine("You entered a valid zip code!");*/
            
         }
     }
